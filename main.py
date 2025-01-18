4a33953dcd58b78dd1b84f8c73804edb65b81b20
Klimpen <axco2004@gmail.com>
2025-01-18 15:19:17 +1300

    initial commit of main.py, constants.py and env requirements

---
+++ main.py
@@ -0,0 +1,36 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    #Player.containers = (updatable, drawable)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+
+        for update in updatable:
+            update.update(dt)
+            
+        for draw in drawable:
+            draw.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
