4747e5e56157d37e72118dd16568b81620a91519
Klimpen <axco2004@gmail.com>
2025-01-19 21:06:43 +1300

    initial commit of vertex gen

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from vertex import *
+
+# create mst
+# display mst
+# place root at time_pos = (0,0)
+# place other vertexes at positions:
+#   time_distance from root
+#       a straight line should be the fastest way from root->vertex
+#       if you have to go root->a->b to go root->b then a should be on the line root->b
+#   in a way which doesn't majorly rearrange the cartesian representation of the map
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
-    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
-    #Player.containers = (updatable, drawable)
+    Vertex.containers = (drawable)
+
+    vertex_factory = Vertex_Factory()
 
     while True:
         for event in pygame.event.get():
@@ -21,16 +28,12 @@ def main():
                 return
         screen.fill("black")
 
-        for update in updatable:
-            update.update(dt)
-            
         for draw in drawable:
             draw.draw(screen)
 
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
---
+++ vertex.py
@@ -0,0 +1,49 @@
+import pygame
+import random
+from constants import*
+
+class Vertex(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+
+        self.position = pygame.Vector2(x, y)
+        self.edges = [] # list of (vertex, distance_in_time)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, 2, width=2)
+        for edge in self.edges:
+            pygame.draw.line(screen,"green", self.position, edge[0].position, width=1)
+
+
+class Vertex_Factory():
+
+    def __init__(self):
+        vertex_list = self.create_vertexes()
+
+    def create_vertexes(self):
+        vertex_list = []
+
+        for i in range(VERTEX_NUM):
+            x = random.random() * SCREEN_WIDTH
+            y = random.random() * SCREEN_HEIGHT
+            vertex_list.append(Vertex(x, y))
+        
+        for i in range(len(vertex_list)):
+            edge_num = random.randint(1, VERTEX_MAX_EDGES)
+            edges_to_make = edge_num - len(vertex_list[i].edges)
+
+            for j in range(i+1, len(vertex_list)):
+
+                ij_distance = vertex_list[i].position.distance_to(vertex_list[j].position)
+
+                if  ij_distance < VERTEX_MAX_LINK_DISTANCE:
+                    vertex_list[i].edges.append((vertex_list[j], ij_distance))
+                    vertex_list[j].edges.append((vertex_list[i], ij_distance))
+                    edges_to_make -= 1
+
+                if edges_to_make <= 0:
+                    break
+            
+        return vertex_list
